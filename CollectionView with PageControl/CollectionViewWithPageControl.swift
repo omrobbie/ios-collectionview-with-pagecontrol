426a472bbdadf1ec65f22657a0238532eb1c43ca
omrobbie <putu.roby@yahoo.com>
2020-04-30 13:30:40 +0800

    set the same height as placeholder view

--- CollectionView with PageControl/CollectionViewWithPageControl.swift
+++ CollectionView with PageControl/CollectionViewWithPageControl.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class CollectionViewWithPageControl: UIViewController {
 
+    @IBOutlet weak var constraintHeight: NSLayoutConstraint!
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     func showView(_ onView: UIView) {
         onView.addSubview(view)
+        constraintHeight.constant = onView.frame.height
         view.translatesAutoresizingMaskIntoConstraints = false
         view.topAnchor.constraint(equalTo: onView.topAnchor).isActive = true
         view.rightAnchor.constraint(equalTo: onView.rightAnchor).isActive = true
